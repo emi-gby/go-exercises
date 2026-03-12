1ec547e854f0f4062db6a8c1176b2f4663a8304c
emi-gby <emitrzs.hub@gmail.com>
2026-03-12 18:28:19 -0300

    Adding reverse_word exercise

---
+++ Basics/reverse_sword/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"	
+
+
+func reverse(word string) string {
+	runes := []rune(word)
+	runesReversed := []rune{}
+
+	for i := (len(runes)-1); i>=0; i-- {
+		runesReversed = append(runesReversed, runes[i])
+	}
+	wordReversed := string(runesReversed)
+	return wordReversed
+}
+
+func main() {
+
+	var word string
+	
+	fmt.Print("Enter a word to reverse : ")
+	fmt.Scanln(&word)
+
+	wordReversed := reverse(word)
+
+	fmt.Println("Result : ", wordReversed)
+}
